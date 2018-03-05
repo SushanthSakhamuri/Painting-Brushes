a6493ea5cefec065dbbcd0803f97f08a7121d33b
SushanthSakhamuri <36965509+SushanthSakhamuri@users.noreply.github.com>
2018-03-04 19:09:12 -0600

    Add files via upload

---
+++ MainWebpage.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class MainWebpage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSelect_Click(object sender, EventArgs e)
+        {
+            string Selection = DropDownList1.SelectedValue;
+           Response.Redirect(Selection);
+                
+
+            
+        }
+    }
+}
